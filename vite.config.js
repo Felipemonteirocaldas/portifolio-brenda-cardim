182ae39beaf6aabe88f9b80e0052f7a98b6348b1
Felipemonteirocaldas <felipemonteiroc@protonmail.com>
2025-08-12 16:38:12 -0300

    fix: Atualiza import do plugin do Tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss/vite'
+// A CORREÇÃO ESTÁ NESTA LINHA:
+import tailwindcss from 'tailwindcss'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // ESSA É A LINHA ADICIONADA PARA O DEPLOY FUNCIONAR
   base: '/portfolio-brenda-cardin/',
-
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
