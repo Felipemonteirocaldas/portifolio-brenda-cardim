5ac788da04e658e60798fe608ddccb86bec4f723
Felipemonteirocaldas <felipemonteiroc@protonmail.com>
2025-08-12 16:29:55 -0300

    feat: Configura o 'base' para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from 'tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  // ESSA É A LINHA ADICIONADA PARA O DEPLOY FUNCIONAR
+  base: '/portfolio-brenda-cardin/',
+
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
